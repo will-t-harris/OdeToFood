472ea923a3ff3465bdc2783b222de1ee63ec7045
Will Harris <will.t.harris@protonmail.com>
2019-11-01 10:50:14 -0600

    feat(restaurants): add page and link in navbar

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
-    public class ListModel : PageModel
+  public class ListModel : PageModel
+  {
+    private readonly IConfiguration config;
+    public string Message { get; set; }
+
+    public ListModel(IConfiguration config)
     {
-        public void OnGet()
-        {
-        }
+      this.config = config;
     }
+    public void OnGet()
+    {
+      Message = config["Message"];
+    }
+  }
 }
