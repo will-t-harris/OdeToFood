f4eb42e8240d80b7dbba50005f87ab265ce69faa
Will Harris <will.t.harris@protonmail.com>
2019-11-01 14:09:15 -0600

    test(loanTest): update test

--- OdeToFood.Data/RestaurantData.cs
+++ OdeToFood.Data/RestaurantData.cs
@@ -5,8 +5,29 @@ using System.Text;
 
 namespace OdeToFood.Data
 {
-  public interface RestaurantData
+  public interface IRestaurantData
   {
-    IEnumerable<Restaurant>
+    IEnumerable<Restaurant> GetAll();
+  }
+
+  public class InMemoryRestaurantData : IRestaurantData
+  {
+    List<Restaurant> restaurants;
+    public InMemoryRestaurantData()
+    {
+      restaurants = new List<Restaurant>()
+      {
+        new Restaurant {Id = 1, Name = "Will's Pizza", Location = "New Mexico", Cuisine = CuisineType.Italian},
+        new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
+        new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
+      };
+    }
+
+    public IEnumerable<Restaurant> GetAll()
+    {
+      return from r in restaurants
+             orderby r.Name
+             select r;
+    }
   }
 }
