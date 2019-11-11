06c845f793e310a24624f95cd8cbd71925114c15
Will Harris <will.t.harris@protonmail.com>
2019-11-11 07:56:04 -0700

    set up edit form and connect with view model

--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,15 +13,19 @@ namespace OdeToFood.Pages.Restaurants
   public class EditModel : PageModel
   {
     private readonly IRestaurantData restaurantData;
+    private readonly IHtmlHelper htmlHelper;
 
     public Restaurant Restaurant { get; set; }
+    public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-    public EditModel(IRestaurantData restaurantData)
+    public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
     {
       this.restaurantData = restaurantData;
+      this.htmlHelper = htmlHelper;
     }
     public IActionResult OnGet(int restaurantId)
     {
+      Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
       Restaurant = restaurantData.GetById(restaurantId);
 
       if (Restaurant == null)
